172a6b386fca0466ed8862f91762a209454fb66d
Gribochek Grant <herrytr@mail.ru>
2026-03-19 22:14:33 +1000

    feat: add connect ot db

--- cmd/main.go
+++ cmd/main.go
@@ -2,19 +2,33 @@ package main
 
 import (
 	"log"
+	"os"
 
 	gotodo "github.com/grancc/go-to-do-app"
 	"github.com/grancc/go-to-do-app/pkg/handler"
 	"github.com/grancc/go-to-do-app/pkg/repository"
-	"github.com/grancc/go-to-do-app/pkg/service"
+	"github.com/grancc/go-to-do-app/pkg/service" 
 	"github.com/spf13/viper"
 )
 
 func main() {
 	if err := initConfig(); err != nil {
 		log.Fatalf("error initialization config: %s", err.Error())
+	} 
+
+	db, err := repository.NewPOstgresDB(repository.Config{
+		Host:     viper.GetString("db.host"),
+		Port:     viper.GetString("db.port"),
+		Username: os.Getenv("POSTGRES_USER"),
+		Password: os.Getenv("POSTGRES_PASSWORD"),
+		DBName:   os.Getenv("POSTGRES_DB"),
+		SSLMode:  viper.GetString("db.sslmode"),
+	})
+	if err != nil {
+		log.Fatalf("failed to initilizqation db: %s", err.Error())
 	}
-	repo := repository.NewRepository()
+
+	repo := repository.NewRepository(db)
 	services := service.NewService(repo)
 	handlers := handler.NewHandler(services)
 	srv := new(gotodo.Server)
